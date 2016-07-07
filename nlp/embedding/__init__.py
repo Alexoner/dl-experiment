20f4551ca9ad4fa5535868c3e2f7495bb414bfab
Alexoner <onerhao@gmail.com>
2016-07-07 15:02:39 +0800

    word2vec utils

--- nlp/embedding/__init__.py
+++ nlp/embedding/__init__.py
@@ -2,13 +2,14 @@ import numpy as np
 from gensim.models import word2vec
 from tqdm import trange
 
+from ..utils import *
 
 def trainW2V(corpus, epochs=1, embed_size=100,
              min_word_count=1, num_workers=8,
              window=10, sample=1e-5):
     """
     @args
-    corpus: list of sequences of tokens
+    corpus: list of list of tokens
     """
     global word2vec
     word2vec = word2vec.Word2Vec(workers=num_workers,
@@ -24,3 +25,4 @@ def trainW2V(corpus, epochs=1, embed_size=100,
         word2vec.alpha *= 0.9
         word2vec.min_alpha = word2vec.alpha
     print("Training word2vec done.")
+    return word2vec
